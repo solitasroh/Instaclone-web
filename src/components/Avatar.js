7cffb87114c4cc51e336004b6dbd842a6290d3c1
solitasroh <solitasroh@gmail.com>
2021-04-15 23:03:13 +0900

    feed 화면 수정 - isLiked

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SAvatar = styled.div`
-	width: 18px;
-	height: 18px;
+	width: ${(props) => (props.lg ? '30px' : '25px')};
+	height: ${(props) => (props.lg ? '30px' : '25px')};
 	border-radius: 15px;
 	background-color: #2c2c2c;
 	overflow: hidden;
 `;
 const Img = styled.img`max-width: 100%;`;
-function Avatar({ url = '' }) {
-	return <SAvatar>{url !== '' ? <img src={url} /> : null}</SAvatar>;
+function Avatar({ url = '', lg = false }) {
+	return <SAvatar lg={lg}>{url !== '' ? <Img src={url} /> : null}</SAvatar>;
 }
 
 export default Avatar;
