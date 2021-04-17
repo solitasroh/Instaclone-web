4e1264a0de52de83953001a1e40e50514662edbd
solitasroh <solitasroh@gmail.com>
2021-04-17 22:53:27 +0900

    Parsing Hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,3 +1,4 @@
+import sanitizeHtml from 'sanitize-html';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -5,13 +6,30 @@ import styled from 'styled-components';
 import { FatText } from '../shared';
 
 const CommentContainer = styled.div`margin-bottom: 10px;`;
-const CommentCaption = styled.span`margin-left: 5px;`;
+const CommentCaption = styled.span`
+	margin-left: 5px;
+	mark {
+		background-color: inherit;
+		color: #03376a;
+		cursor: pointer;
+		text-decoration: underline;
+	}
+`;
+
+const replacePayload = (payload) => {
+	const regex = /#[\w]+/g;
+	const result = sanitizeHtml(payload.replace(regex, '<mark>$&</mark>'), {
+		allowedTags: [ 'mark' ]
+	});
+	console.log(result);
+	return result;
+};
 
 function Comment({ author, payload }) {
 	return (
 		<CommentContainer>
 			<FatText>{author}</FatText>
-			<CommentCaption>{payload}</CommentCaption>
+			<CommentCaption dangerouslySetInnerHTML={{ __html: replacePayload(payload) }} />
 		</CommentContainer>
 	);
 }
